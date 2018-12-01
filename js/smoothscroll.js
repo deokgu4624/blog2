baeb34aec1f414005a2f6b33b9af65d342888e10
deokgu4624 <deokgu4624@gmail.com>
2018-12-02 02:30:39 +0900

    .

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -3,7 +3,7 @@ $(function(){
 	var $window = $(window);		//Window object
 	
 	var scrollTime = 1.2;			//Scroll time
-	var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+	var scrollDistance = 300;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
 		
 	$window.on("mousewheel DOMMouseScroll", function(event){
 		
